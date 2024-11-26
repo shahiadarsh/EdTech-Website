3feaa0e38f74157a68da49bd92fbafb8bd0556fc
shahiadarsh <141950380+shahiadarsh@users.noreply.github.com>
2024-11-26 18:04:34 +0530

    Update UpdatePassword.jsx

--- src/components/core/Dashboard/Settings/UpdatePassword.jsx
+++ src/components/core/Dashboard/Settings/UpdatePassword.jsx
@@ -1,109 +1,126 @@
-import React, { useState } from "react"
-import { useForm } from "react-hook-form"
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
-import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-
-import { changePassword } from "../../../../services/operations/SettingsAPI"
-import IconBtn from "../../../common/IconBtn"
+import React from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { updatePassword} from '../../../../services/operations/profileAPI'
+import { AiOutlineEyeInvisible } from 'react-icons/ai'
+import { AiOutlineEye } from 'react-icons/ai'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export default function UpdatePassword() {
-  const { token } = useSelector((state) => state.auth)
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.profile.user);
 
-  const [showOldPassword, setShowOldPassword] = useState(false)
-  const [showNewPassword, setShowNewPassword] = useState(false)
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
-
-  const submitPasswordForm = async (data) => {
-    // console.log("password Data - ", data)
-    try {
-      await changePassword(token, data)
-    } catch (error) {
-      console.log("ERROR MESSAGE - ", error.message)
-    }
-  }
+   //update password
+   const [showPassword, setShowPassword] = useState(false);
+   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+   const token = useSelector(state => state.auth.token);
+   const [password, setPassword] = useState({
+     oldPassword: "",
+     newPassword: "",
+     confirmPassword: "",
+   })
+ 
+   const handleOnChangePassword = (e) => {
+     setPassword((prevData) => ({
+       ...prevData,
+       [e.target.name]: e.target.value,
+     }))
+   }
+ 
+   const handlePassword = (e) => {
+     e.preventDefault()
+     const { newPassword, confirmPassword } = password;
+     if (newPassword === confirmPassword) {
+       updatePassword(token, password);
+     } else {
+       alert("Password does not match")
+     }
+   }
 
   return (
     <>
-      <form onSubmit={handleSubmit(submitPasswordForm)}>
-        <div className="my-10 flex flex-col gap-y-6 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
-          <h2 className="text-lg font-semibold text-richblack-5">Password</h2>
-          <div className="flex flex-col gap-5 lg:flex-row">
-            <div className="relative flex flex-col gap-2 lg:w-[48%]">
-              <label htmlFor="oldPassword" className="lable-style">
-                Current Password
-              </label>
+      {/* update Password */}
+      <form onSubmit={handlePassword}>
+        <div >
+          <div className=' relative mt-4'>
+            <label className="w-full"><p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">Old Password <sup className="text-pink-200">*</sup></p>
               <input
-                type={showOldPassword ? "text" : "password"}
+                required
+                type={showPassword ? "text" : "password"}
                 name="oldPassword"
-                id="oldPassword"
-                placeholder="Enter Current Password"
-                className="form-style"
-                {...register("oldPassword", { required: true })}
-              />
-              <span
-                onClick={() => setShowOldPassword((prev) => !prev)}
-                className="absolute right-3 top-[38px] z-[10] cursor-pointer"
-              >
-                {showOldPassword ? (
-                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
-                ) : (
-                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
-                )}
-              </span>
-              {errors.oldPassword && (
-                <span className="-mt-1 text-[12px] text-yellow-100">
-                  Please enter your Current Password.
-                </span>
+                value={password.oldPassword}
+                onChange={handleOnChangePassword}
+                placeholder="Enter Password"
+                style={{
+                  boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                }}
+                className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] pr-12 text-richblack-5"
+              /></label>
+            <span
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-3 top-9 z-[10] cursor-pointer"
+            >
+              {showPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" color="white" className='' />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" color="white" />
               )}
-            </div>
-            <div className="relative flex flex-col gap-2 lg:w-[48%]">
-              <label htmlFor="newPassword" className="lable-style">
-                New Password
-              </label>
+            </span>
+          </div>
+          <div className=' relative mt-4'>
+            <label className="w-full"><p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">New Password <sup className="text-pink-200">*</sup></p>
               <input
-                type={showNewPassword ? "text" : "password"}
+                required
+                type={showConfirmPassword ? "text" : "password"}
                 name="newPassword"
-                id="newPassword"
-                placeholder="Enter New Password"
-                className="form-style"
-                {...register("newPassword", { required: true })}
-              />
-              <span
-                onClick={() => setShowNewPassword((prev) => !prev)}
-                className="absolute right-3 top-[38px] z-[10] cursor-pointer"
-              >
-                {showNewPassword ? (
-                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
-                ) : (
-                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
-                )}
-              </span>
-              {errors.newPassword && (
-                <span className="-mt-1 text-[12px] text-yellow-100">
-                  Please enter your New Password.
-                </span>
+                value={password.newPassword}
+                onChange={handleOnChangePassword}
+                placeholder="Enter Password"
+                style={{
+                  boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                }}
+                className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] pr-12 text-richblack-5"
+              /></label>
+            <span
+              onClick={() => setShowConfirmPassword((prev) => !prev)}
+              className="absolute right-3 top-9 z-[10] cursor-pointer"
+            >
+              {showConfirmPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" color="white" className='' />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" color="white" />
               )}
-            </div>
+            </span>
+          </div>
+          <div className=' relative mt-4'>
+            <label className="w-full"><p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">Confirm New Password <sup className="text-pink-200">*</sup></p>
+              <input
+                required
+                type={showConfirmPassword ? "text" : "password"}
+                name="confirmPassword"
+                value={password.confirmPassword}
+                onChange={handleOnChangePassword}
+                placeholder="Enter Password"
+                style={{
+                  boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                }}
+                className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] pr-12 text-richblack-5"
+              /></label>
+            <span
+              onClick={() => setShowConfirmPassword((prev) => !prev)}
+              className="absolute right-3 top-10 z-[10] cursor-pointer"
+            >
+              {showConfirmPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" color="white" className='' />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" color="white" />
+              )}
+            </span>
           </div>
         </div>
-        <div className="flex justify-end gap-2">
-          <button
-            onClick={() => {
-              navigate("/dashboard/my-profile")
-            }}
-            className="cursor-pointer rounded-md bg-richblack-700 py-2 px-5 font-semibold text-richblack-50"
-          >
-            Cancel
-          </button>
-          <IconBtn type="submit" text="Update" />
-        </div>
+        <div className="flex justify-end gap-2 mt-3"><button className="flex items-center bg-yellow-50 cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 undefined" type="submit">Save</button></div>
       </form>
     </>
   )
